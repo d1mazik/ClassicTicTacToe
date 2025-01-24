ba9a6d32808ce6998268ccac891bcef243d30fed
Dmitri Gustafsson <dima.gustafsson@hotmail.com>
2025-01-24 11:18:59 +0100

    Refactor: Fix UI and game logic integration

--- src/main/java/com/example/classictictactoe/HelloController.java
+++ src/main/java/com/example/classictictactoe/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.classictictactoe;
 
 import java.net.URL;
-import java.nio.charset.CharsetEncoder;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,6 +17,9 @@ public class HelloController {
     @FXML
     private URL location;
 
+    @FXML
+    private GridPane gridPane;
+
     private char nowSym = 'x';
 
     private char gameField[][] = new char[3][3];
@@ -27,67 +29,87 @@ public class HelloController {
     @FXML
     void btnClick(ActionEvent event) {
 
-        Button btn = (Button)event.getSource();
+        Button btn = (Button) event.getSource();
 
-        if(!isGame || btn.getText() != "") return;
+        if (!isGame || !btn.getText().isEmpty()) return;
 
         int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
         int columnIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
-        //System.out.println(rowIndex);
-        //System.out.println(columnIndex);
 
         gameField[rowIndex][columnIndex] = nowSym;
 
         btn.setText(String.valueOf(nowSym));
 
-        if(gameField[0][0] == gameField[0][1] && gameField[0][0] == gameField[0][2] && (gameField[0][0] == 'x' || gameField[0][0] == 'o' ) ){
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
+        if (checkWinner()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + nowSym + ">>", ButtonType.OK);
+            alert.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    resetGame();
+                }
+            });
+        } else if (isBoardFull()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "It's a draw!", ButtonType.OK);
+            alert.showAndWait().ifPresent(response -> {
+                if (response == ButtonType.OK) {
+                    resetGame();
+                }
+            });
+        } else {
+            nowSym = nowSym == 'x' ? 'o' : 'x';
         }
-        else if(gameField[1][0] == gameField[1][1] && gameField[1][0] == gameField[1][2] && (gameField[1][0] == 'x' || gameField[1][0] == 'o' ) ) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
-        else if(gameField[2][0] == gameField[2][1] && gameField[2][0] == gameField[2][2] && (gameField[2][0] == 'x' || gameField[2][0] == 'o' ) ) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
-        else if(gameField[0][0] == gameField[1][0] && gameField[0][0] == gameField[2][0] && (gameField[0][0] == 'x' || gameField[0][0] == 'o' ) ) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
-        else if(gameField[0][1] == gameField[1][1] && gameField[0][1] == gameField[2][1] && (gameField[0][1] == 'x' || gameField[0][1] == 'o' ) ) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
-        else if(gameField[0][2] == gameField[1][2] && gameField[0][2] == gameField[2][2] && (gameField[0][2] == 'x' || gameField[0][2] == 'o' ) ) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
-        else if(gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2] && (gameField[0][0] == 'x' || gameField[0][0] == 'o' ) ) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
-        else if(gameField[2][0] == gameField[1][1] && gameField[2][0] == gameField[0][2] && (gameField[2][0] == 'x' || gameField[2][0] == 'o' ) )
-        {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner <<" + btn.getText() + ">>", ButtonType.OK);
-            alert.showAndWait();
-            isGame = false;
-        }
+    }
 
-        nowSym = nowSym == 'x' ? 'o' : 'x';
+    private boolean checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            // Check rows
+            if (gameField[i][0] == gameField[i][1] && gameField[i][0] == gameField[i][2] && gameField[i][0] != '\0') {
+                return true;
+            }
+            // Check columns
+            if (gameField[0][i] == gameField[1][i] && gameField[0][i] == gameField[2][i] && gameField[0][i] != '\0') {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2] && gameField[0][0] != '\0') {
+            return true;
+        }
+        if (gameField[0][2] == gameField[1][1] && gameField[0][2] == gameField[2][0] && gameField[0][2] != '\0') {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameField[i][j] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                gameField[i][j] = '\0';
+            }
+        }
+        nowSym = 'x';
+        isGame = true;
+
+        // Clear all button texts
+        gridPane.getChildren().forEach(node -> {
+            if (node instanceof Button) {
+                ((Button) node).setText("");
+            }
+        });
     }
 
     @FXML
     void initialize() {
-
+        resetGame();
     }
-
 }
